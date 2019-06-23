70c3d65e31c0b975385b6f5bb89cc086ec220ba0
1246650576 <1246650576@qq.com>
2019-06-23 12:11:50 +0800

    修复STime.cpp中的bug,(CurrentTime.wDayOfWeek取值0-6表示星期日至星期六)

--- Student Information Management System/STime.cpp
+++ Student Information Management System/STime.cpp
@@ -20,7 +20,7 @@ void Time::getTime()
 }
 //显示当前时间
 void Time::showTime() {
-	const char* WEEK[7] = {"星期一","星期二","星期三","星期四","星期五","星期六","星期日"};
+	const char* WEEK[7] = {"星期日","星期一","星期二","星期三","星期四","星期五","星期六"};
 	getTime();
 	int ssecond = second;
 	do {
@@ -28,7 +28,7 @@ void Time::showTime() {
 		cout << setw(2) << setfill('0') << hour << ":"
 			<< setw(2) << minute << ":"
 			<< setw(2) << second << setfill(' ')<< endl;
-		cout << year << "年" << month << "月" << day << "日" << "   " << WEEK[week - 1] << endl;
+		cout << year << "年" << month << "月" << day << "日" << "   " <<  WEEK[week] << endl;
 		cout << endl << endl << endl << endl << "      欢迎使用学生信息管理系统" << endl << endl
 			<< "              启动中..." << endl;
 
